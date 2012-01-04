aa0ad6eb7ab05fd0716efb9946eeecc3a6976527
Sergey Dorofeev <sergey@fidoman.ru>
2012-01-04 13:33:25 +0400

    Message exporter (outgoing flow)

---
+++ ftnexport.py
@@ -0,0 +1,10 @@
+"""
+Message flow from database to links and users
+"""
+
+import ftn.msg
+
+def denormalize_message((origdom, origaddr), (destdom, destaddr), msgid, header, body):
+  raise Exception("not implemented")
+  return #ftn.msg.MSG
+
