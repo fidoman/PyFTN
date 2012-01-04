be66aad00199dc3d828115788e51b22c661d0077
Sergey Dorofeev <sergey@fidoman.ru>
2012-01-04 19:38:07 +0400

    inbound parameter

--- ftnconfig.py
+++ ftnconfig.py
@@ -1,5 +1,36 @@
 
+import postgresql
+
+db=postgresql.open("pq://fido:bgdis47wb@192.168.0.3/fido")
 
 DUPDIR="dupmsg"
 BADDIR="badmsg"
 SECDIR="secmsg"
+ADDRESS="2:5020/12000"
+INBOUND="/tank/home/sergey/fido/fidotmp/archive"
+
+
+# - values configured in database
+
+def init_domains(db):
+  try:
+    return db.FTN_domains, db.FTN_backdomains
+  except AttributeError:
+    db.FTN_domains = {}
+    db.FTN_backdomains = {}
+    for domain_id, domain_name in db.prepare("SELECT id, name FROM domains"):
+      if domain_name=="fidonet node":
+        db.FTN_domains["node"]=domain_id
+        db.FTN_backdomains[domain_id]="node"
+      elif domain_name=="fidonet echo":
+        db.FTN_domains["echo"]=domain_id
+        db.FTN_backdomains[domain_id]="echo"
+      elif domain_name=="fidonet fileecho":
+        db.FTN_domains["fileecho"]=domain_id
+        db.FTN_backdomains[domain_id]="fileecho"
+      elif domain_name=="fidonet backbone":
+        db.FTN_domains["backbone"]=domain_id
+        db.FTN_backdomains[domain_id]="backbone"
+    return db.FTN_domains, db.FTN_backdomains
+
+init_domains(db)
