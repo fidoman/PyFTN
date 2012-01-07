5a233c50a93b1b2bd3c48668b1ee441dce8ed419
Sergey Dorofeev <sergey@fidoman.ru>
2012-01-07 15:00:11 +0400

    listener

--- ftndaemon.py
+++ ftndaemon.py
@@ -27,3 +27,44 @@
     > (data)
     < OK
     then repeat or close connection if done or go to outbound processing
+"""
+
+import socket
+import select
+import threading
+
+from ftnconfig import *
+
+def readline(s):
+  l=b""
+  while True:
+    c=s.recv(1)
+    if c==b"\n":
+      break
+    l+=c
+  return l
+
+def session(s, a):
+  s.send(b"hi "+str(a).encode("utf-8")+b"\n")
+  print(readline(s).decode("utf-8"))
+  s.close()
+
+sockets=[]
+
+for af, addr in DAEMONBIND:
+  sockets.append((socket.socket(af), addr))
+
+for s, a in sockets:
+  s.bind(a)
+  s.listen(3)
+
+threads = []
+
+while True:
+  (incoming_connections, _, _) = select.select([x[0] for x in sockets], [], [])
+  for ic in incoming_connections:
+    ls, peer = ic.accept()
+    print(ls, peer)
+    t=threading.Thread(target=session, args=(ls,peer))
+    threads.append(t)
+    t.start()
