984c97da94f3ee03907af5a9040e76da9fdff199
Sergey Dorofeev <sergey@fidoman.ru>
2012-01-21 20:25:18 +0400

    local netmail saver and area/filefix

--- ftnlocal.py
+++ ftnlocal.py
@@ -4,13 +4,122 @@
 
 from ftnconfig import *
 import ftnexport
+import ftnimport
+import time
+
+from badwriter import BadWriter
+
+localmsgs=BadWriter(LOCALNETMAIL, os.path.join(LOCALNETMAIL, "next"), "msg")
+
+def fix(db, sess, src, domain, password, cmdtext):
+  dom, text = db.prepare("select domain, text from addresses where id=$1").first(src)
+  if dom != db.FTN_domains["node"]:
+    raise FTNFail("not our domain")
+  print(text)
+  if password != get_link_password(db, text):
+    print("wrong password")
+  else:
+    reply=[]
+    for cmd in map(str.strip, cmdtext.upper().strip().split("\n")):
+      if cmd.startswith("---"):
+        break
+      elif len(cmd)==0:
+        continue
+      elif cmd.startswith("%RESCAN "):
+        reply.append("Rescan not implemented, contact sysop: "+cmd)
+      elif cmd.startswith("%PING"):
+        reply.append("NOP: "+cmd)
+      elif cmd.startswith("%QUERY"):
+        reply.append("Sorry not implemented: "+cmd)
+      elif cmd.startswith("%INFO"):
+        reply.append("Sorry not implemented: "+cmd)
+      elif cmd.startswith("%HELP"):
+        reply.append("Sorry not implemented: "+cmd)
+      elif cmd.startswith("%LIST"):
+        reply.append("Sorry not implemented: "+cmd)
+      elif cmd.startswith("%AVAIL"):
+        reply.append("Sorry not implemented: "+cmd)
+      elif cmd.startswith("-"):
+        reply.append(sess.remove_subscription(domain, cmd[1:], text) + ": " + cmd)
+      elif cmd.startswith("+"):
+        reply.append(sess.add_subscription(False, domain, cmd[1:], text) + ": " + cmd)
+      elif cmd.startswith("%"):
+        reply.append("Unknown command: "+cmd)
+        print(cmd)
+        1/0
+      else:
+        reply.append(sess.add_subscription(False, domain, cmd, text) + ": " + cmd)
+    print(reply)  #  sess.send_message("robot", ("node", ADDRESS), "sysop", "test", "test message")
+    # .. send reply
+
 
 #for s in ftnexport.get_node_subscriptions(db, "2:5020/4441", "echo"):
 #  print(db.prepare("select a.domain, a.text from addresses a, subscriptions s where s.id=$1 and a.id=s.target")(s)[0])
 #exit()
 
+me = db.prepare("select id from addresses where domain=$1 and text=$2").first(db.FTN_domains["node"], ADDRESS)
+
+fixes_e = []
+fixes_f = []
+my = []
+
+
+for id_msg, src, dest, msgid, header, body, recvfrom in ftnexport.get_subscriber_messages_n(db, me, db.FTN_domains["node"]):
+  if header.find("recipientname").text.lower() == "areafix":
+    fixes_e.append((id_msg, src, time.mktime(time.strptime(header.find("date").text, "%d %b %y  %H:%M:%S")), header.find("subject").text, body))
+  elif header.find("recipientname").text.lower() == "filefix":
+    fixes_f.append((id_msg, src, time.mktime(time.strptime(header.find("date").text, "%d %b %y  %H:%M:%S")), header.find("subject").text, body))
+  elif header.find("recipientname").text.lower() == "allfix":
+    fixes_f.append((id_msg, src, time.mktime(time.strptime(header.find("date").text, "%d %b %y  %H:%M:%S")), header.find("subject").text, body))
+  else:
+
+    srca=db.prepare("select domain, text from addresses where id=$1").first(src)
+    dsta=db.prepare("select domain, text from addresses where id=$1").first(dest)
+
+    print(srca)
+
+    m = ftnexport.denormalize_message(
+        (db.FTN_backdomains[srca[0]], srca[1]),
+        (db.FTN_backdomains[dsta[0]], dsta[1]),
+        msgid, header, body)
+
+    my.append((id_msg, time.mktime(time.strptime(header.find("date").text, "%d %b %y  %H:%M:%S")), m))
+
+    print("*************"+str(id_msg)+"*"+msgid+"*****************")
+    print("From:", header.find("sendername").text, src)
+    print("To  :", header.find("recipientname").text, dest)
+    print("Date:", header.find("date").text)
+    print("Subj:", header.find("subject").text)
+#    print(body)
+    # process
+    # if fail abort
+    # if ok update watermark update_subscription_watermark(db, localnetmailsubscription, id_msg)
+
+
+fixes_e.sort(key = lambda x: x[2])
+fixes_f.sort(key = lambda x: x[2])
+my.sort(key = lambda x: x[1])
+
+for id_msg, _, m in my:
+  localmsgs.write(m.pack(), None)
+  db.prepare("update messages set processed=1 where id=$1")(id_msg)
+
+for id_msg, src, _, h, b in fixes_e:
+  with ftnimport.session(db) as sess:
+    fix(db, sess, src, db.FTN_domains["echo"], h, b)
+    db.prepare("update messages set processed=1 where id=$1")(id_msg)
+    
+
+#for id_msg, src, _, h, b in fixes_f:
+#  fix(db, src, db.FTN_domains["fileecho"], h, b)
+
+exit()
+
 #for s in ftnexport.get_node_subscriptions(db, "2:5020/12000", "node"):
 #  print(db.prepare("select a.domain, a.text from addresses a, subscriptions s where s.id=$1 and a.id=s.target")(s)[0])
+
+
+
 
 for sub_id, target_id, lastsent in ftnexport.get_node_subscriptions(db, ADDRESS, "node"):
   print(sub_id, ": mail directed to", 
