72514901aa50e2e6b9e8e6053a443684f5b21a0a
Sergey Dorofeev <sergey@fidoman.ru>
2012-02-21 23:42:51 +0400

    export passwords and subscriptions to files

---
+++ export_bbs.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3 -bb
+
+from ftnconfig import connectdb, get_link_password
+
+db=connectdb()
+
+fareas={}
+
+for a, b in db.prepare("select t.text, sr.text from subscriptions s, addresses sr, addresses t "
+            "where s.subscriber=sr.id and s.target=t.id and t.domain=3")():
+  fareas.setdefault(a, set()).add(b)
+
+fa=open("/tank/home/fido/fareas.bbs", "w")
+fa.write("*****\n")
+for k, v in fareas.items():
+  fa.write("/tank/home/fido/fareas/"+k.lower()+" "+k+" "+" ".join(v)+"\n")
+fa.close()
+
+pw=open("/tank/home/fido/passwd", "w")
+for a in db.prepare("select a.text from links l, addresses a "
+            "where l.address=a.id and a.domain=1")():
+  pw.write("password %-23s %s\n"%(a[0], get_link_password(db, a[0])))
+pw.close()
