b7c42a8152997479e259ad0e65b737006d0f707b
Sergey Dorofeev <sergey@fidoman.ru>
2012-02-21 23:44:05 +0400

    resend utility

---
+++ util_goback.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3 -bb
+
+LINK="2:5020/758"
+#ECHO="$CRACK$%"
+ECHO='%'
+
+import ftnconfig
+
+db=ftnconfig.connectdb()
+
+
+
+for i, l, t in db.prepare("""select s.id, s.lastsent, t.text from subscriptions s, addresses sr, addresses t 
+                where s.target=t.id and t.text LIKE $1 and t.domain=$2 and s.subscriber=sr.id and sr.text=$3 and sr.domain=$4""")\
+                    (ECHO, db.FTN_domains["echo"], LINK, db.FTN_domains["node"]):
+    print(i, l, t)
+    db.prepare("update subscriptions set lastsent=1162221 where id=$1")(i)
