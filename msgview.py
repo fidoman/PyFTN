527de49f707601d4541efdd497972925eb5ab43c
Sergey Dorofeev <sergey@fidoman.ru>
2012-02-26 09:19:20 +0400

    11

--- msgview.py
+++ msgview.py
@@ -1,10 +1,15 @@
 #!/usr/local/bin/python3
 
 import sys
+import getopt
 import ftn.msg
 
 #f=open(sys.argv[1], "rb")
 m=ftn.msg.MSG(sys.argv[1])
+if len(sys.argv)==3:
+  charset = sys.argv[2]
+else:
+  charset = "cp866"
 #f.close()
 
-print(m.as_str().decode("cp866"))
+print(m.as_str().decode(charset))
