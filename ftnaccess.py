de0ed31dbf4a1e5769162fb7b22dd6193425c879
Sergey Dorofeev <sergey@fidoman.ru>
2014-07-13 11:43:04 +0400

    ftn-style pw check (8 chrs)

--- ftnaccess.py
+++ ftnaccess.py
@@ -12,3 +12,13 @@ def may_post(db, node, address):
   addr_id = ftnconfig.get_addr_id(db, db.FTN_domains[address[0]], address[1])
   x=db.prepare("select id from subscriptions where subscriber=$1 and target=$2")(node_id, addr_id)
   return len(x)>0
+
+def check_pw(genuine, provided):
+  if provided==genuine:
+    return True
+  if len(provided)==8 and provided==genuine[:8]: # if genuine is shorter than 8 chrs provided also must have the same length
+    # it can match only if provided passwd is equal to genuine (if genuine is shorter than 8) or if it is equal to first 8 chars
+    return True
+  if len(provided)==8 and provided==genuine[:8].upper():
+    return True
+  return False
