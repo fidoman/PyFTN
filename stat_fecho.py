2795ffdc131c1cb9e36d40a2bc16cb22c572007f
Sergey Dorofeev <sergey@fidoman.ru>
2014-11-25 21:41:24 +0100

    fileecho stats

--- stat_fecho.py
+++ stat_fecho.py
@@ -1,65 +1,51 @@
 #!/usr/local/bin/python3 -bb
 
-LOG="/tank/home/fido/tic.log"
-OLDLOG="/tank/home/fido/tic.log.old"
-
 import re
 import os
 import ftnconfig
 import ftnimport
 
-spc=re.compile("\s+")
+import datetime
 
-if not os.path.exists(LOG):
-    exit()
+today=datetime.datetime.now(datetime.timezone.utc)
+h=today.hour
+m=today.minute
+s=today.second
+mks=today.microsecond
+x=datetime.timedelta(seconds=h*3600+m*60+s, microseconds=mks)
 
-byarea={}
+today = today-x
+
+yesterday=today-datetime.timedelta(1)
+
+#print(today)
+#print(yesterday)
 
 lines=[]
-for l in open(LOG, encoding="cp866"): # data from tics is copied as is though it is bad way
-    lines.append(l)
-    a=list(map(str.strip, spc.split(l)))
-    date=a[0]
-    time=a[1]
-    a=a[2:]
-    src=None
-    file=None
-    area=None
-    while len(a):
-        if a[0]=="from":
-            src=a[1]
-        elif a[0]=="file":
-            file=a[1]
-        elif a[0]=="area":
-            area=a[1]
-        a=a[2:]
-    byarea.setdefault(area, []).append((file, src))
 
-
-areas=list(byarea.keys())
-areas.sort()
-
-outp = []
-
-for area in areas:
-#  outp.append ("AREA: "+ area+"\n")
-  for file, src in byarea[area]:
-    outp.append ("%-20s %-32s %-23s\n"%(area, file, src))
-  outp.append("\n")
+prv_area=None
 
 db=ftnconfig.connectdb()
+for area, fname, fsize, recv_timestamp, recv_from in db.prepare(
+    "select (select text from addresses where id=fp.destination), fp.filename, "
+           "(select length from files where id=fp.filedata), "
+           "fp.recv_timestamp, "
+           "(select text from addresses where id=fp.recv_from) "
+    "from file_post fp, addresses a "
+    "where a.id=fp.destination and fp.recv_timestamp>=$1 and fp.recv_timestamp<$2 "
+    "order by a.text, fp.filename")(yesterday, today):
+
+    if prv_area is not None and prv_area!=area:
+      lines.append("\n")
+      prv_area = area
+
+    lines.append("%-20s %-22s %-9d %-23s\n"%(area, fname, fsize, recv_from))
+
+#    print(lines[-1])
+#exit()
 
 with ftnimport.session(db) as sess:
   sess.send_message(("node", ftnconfig.ADDRESS), "Sergey Dorofeev", ("echo", "FLUID.REPORTS"), "All", None, "файлэхи",
 """
 %s
-"""%("".join(outp)))
-
-fo=open(OLDLOG, "a", encoding="cp866")
-for l in lines:
-    fo.write(l)
-fo.close
-os.unlink(LOG)
-
-
-#2012-02-21 08:30:12 from 2:5020/1200 file PNT5025.ZIP area PNTLIST replaces PNT5025.ZIP
+"""%("".join(lines)))
