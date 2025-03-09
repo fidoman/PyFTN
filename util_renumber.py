e9260bb00492cc823a7e0a6c516944cbd25810c2
Sergey Dorofeev <sergey@fidoman.ru>
2025-03-09 15:14:28 +0300

    address dedup tool

--- util_renumber.py
+++ util_renumber.py
@@ -10,22 +10,52 @@ safecount= 0
 safelimit=15000
 safedelay=20
 
-OLD=17240
-NEW=18022
+def get_duplicate_addresses():
+  q=db.prepare("select domain, text, array_agg(id) from addresses group by (domain, text) having count(*)>1")
+  for d, t, ids in q():
+    ids=list(ids)
+    ids.sort()
+    good_id=ids[0]
+    bad_ids=ids[1:]
+    print(d,t,good_id,bad_ids)
+    for bad_id in bad_ids:
+      print("renumber", bad_id, "after", good_id)
+      renumber_dest(good_id, bad_id)
+      print("replace dest",bad_id, "with", good_id, "in messages")
+      print(db.prepare("update messages set id=DEFAULT, destination=$1 where destination=$2")(good_id, bad_id))
+      print("replace dest",bad_id, "with", good_id, "in subscriptions")
+      for s_id, s_s in db.prepare("select id, subscriber from subscriptions where target=$1")(bad_id):
+        print(s_s,"subscribed to bad", bad_id)
+        if len(db.prepare("select id, subscriber from subscriptions where subscriber=$1 and target=$2")(s_s, good_id))>0:
+          print(s_s, "already subscribed to", good_id)
+          print(db.prepare("delete from subscriptions where id=$1")(s_id))
+        else:
+          print(db.prepare("update subscriptions set target=$1 where target=$2")(good_id, bad_id))
 
-if OLD>=NEW:
-  print("go fuck yourself")
-  exit(1)
+      print("delete", bad_id)
+      exit()
+      print(db.prepare("delete from addresses where id=$1")(bad_id))
+      exit()
+
+
+
 # renumber NEW as continue of OLD
 
 def get_last_numberfordestination_of(dest_id):
   return db.prepare("select max(numberfordestination) from messages where destination=$1").first(dest_id)
 
-print("start from:", get_last_numberfordestination_of(OLD))
 
-number_from=get_last_numberfordestination_of(OLD)+1
+def renumber_dest(OLD,NEW):
+  """ OLD - good valid id
+      NEW - id to merge """
 
-if True:
+  if OLD>=NEW:
+    print("go fuck yourself")
+    exit(1)
+
+  print("start from:", get_last_numberfordestination_of(OLD))
+
+  number_from=get_last_numberfordestination_of(OLD)+1
 
   dest_to_renumber=NEW
 
@@ -43,6 +73,8 @@ if True:
 #      safecount+=1
   xact.commit()
 
+get_duplicate_addresses()
+exit()
 
 exit()
 
